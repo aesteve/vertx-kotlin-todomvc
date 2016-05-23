7869c92408f327fce79a3693d8e425aef331d3b5
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-05-23 23:06:00 +0200

    public port for cloud deployment

--- src/main/kotlin/com/github/aesteve/vertx/kotlin/todomvc/env/HttpEnv.kt
+++ src/main/kotlin/com/github/aesteve/vertx/kotlin/todomvc/env/HttpEnv.kt
@@ -12,6 +12,16 @@ get() {
     }
 }
 
+val PUBLIC_PORT: Int
+    get() {
+        val systemPort = System.getenv("PORT")
+        when (systemPort) {
+            null -> return DEFAULT_PORT
+            else -> return 80 // means we're on the cloud
+        }
+    }
+
+
 val HOST: String
 get() {
     return System.getenv("HOST") ?: DEFAULT_HOST
