501df7a14eeb95812262228ef30b01d2fbcf7959
Arnaud Esteve <arnaud.esteve@gmail.com>
2016-05-23 23:00:00 +0200

    no hostname

--- src/main/kotlin/com/github/aesteve/vertx/kotlin/todomvc/Server.kt
+++ src/main/kotlin/com/github/aesteve/vertx/kotlin/todomvc/Server.kt
@@ -1,11 +1,9 @@
 package com.github.aesteve.vertx.kotlin.todomvc
 
-import com.github.aesteve.vertx.kotlin.todomvc.env.HOST
 import com.github.aesteve.vertx.kotlin.todomvc.env.PORT
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.http.HttpServer
-import io.vertx.core.http.HttpServerOptions
 import io.vertx.ext.web.Router
 
 
@@ -17,9 +15,9 @@ class Server : AbstractVerticle() {
     }
 
     override fun start(future: Future<Void>) {
-        server = vertx.createHttpServer(HttpServerOptions().setPort(PORT).setHost(HOST))
+        server = vertx.createHttpServer()
         server?.requestHandler { router.accept(it) } // no method reference :( https://github.com/Kotlin/KEEP/issues/5
-        server?.listen { result ->
+        server?.listen(PORT) { result ->
             if (result.succeeded()) {
                 future.complete()
             } else {
